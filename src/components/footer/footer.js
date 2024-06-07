f7bdbdfee4732d9d912638e0ee9935894ab352ef
BrandgrandRealMe <brandon@brandgrand.tech>
2024-06-07 08:49:17 -0500

    Added source to footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-
+import { faFileArchive } from '@fortawesome/free-regular-svg-icons';
 const Footer = () => {
     return (
         <div>
             <footer className="footer">
-                Made with <FontAwesomeIcon className="icon" icon={faHeart} /> by Brandon!
+                    Made with <FontAwesomeIcon className="icon" icon={faHeart} /> to be open source by Brandon! {" "}
+                <div className="links">
+                    <Link className="link" href={"/"}>
+                        <FontAwesomeIcon className="icon" icon={faFileArchive} /> Source
+                    </Link>
+                </div>
             </footer>
         </div>
     );
