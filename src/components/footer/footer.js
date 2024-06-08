a85c6eb71bb92f4baeb5946537d1b427f1e5f20f
BrandgrandRealMe <brandon@brandgrand.tech>
2024-06-08 09:24:36 -0500

    Made footer responsive

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
                     Made with <FontAwesomeIcon className="icon" icon={faHeart} /> to be open source by Brandon! {" "}
                 <div className="links">
                     <Link className="link" href="https://github.com/BrandgrandRealMe/Portfolio" target="_blank">
-                        <FontAwesomeIcon className="icon" icon={faGithub} /> Source
+                        <FontAwesomeIcon className="icon" icon={faGithub} /> <div>Source</div>
                     </Link>
                     <Link className="link" href="https://ko-fi.com/brandonthedev" target="_blank">
-                        <FontAwesomeIcon className="icon" icon={faDonate} /> Donate
+                        <FontAwesomeIcon className="icon" icon={faDonate} /> <div>Donate</div>
                     </Link>
                 </div>
             </footer>
