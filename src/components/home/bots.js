8d08d46142e1e0ca476c763df55535961f419953
BrandgrandRealMe <brandon@brandgrand.tech>
2024-06-07 19:27:15 -0500

    Made more responsive

--- src/components/home/bots.js
+++ src/components/home/bots.js
@@ -13,16 +13,20 @@ const Bots = () => {
             <h2 id="Bots">Some Of My Bots</h2>
             <div className="discord">
                 <Link href="https://top.gg/bot/745786473554378832" target="_blank" className="Bot">
-                    <FontAwesomeIcon className="icon" icon={faDiscord} />
-                    <Image src={CyberbotPFP} alt="A pixel art of a robot head" className="pfp circle" />
+                    <div className="icons">
+                        <FontAwesomeIcon className="icon" icon={faDiscord} />
+                        <Image src={CyberbotPFP} alt="A pixel art of a robot head" className="pfp circle" />
+                    </div>
                     <div className="desc">
                         <h2>CYBΞRBot</h2>
                         He has slash commands, You can build a simple embed message, you can get random posts from a few subreddits, And more!
                     </div>
                 </Link>
                 <Link href="https://top.gg/bot/1232768387399024732" target="_blank" className="Bot">
-                    <FontAwesomeIcon className="icon" icon={faDiscord} />
-                    <Image src={TBAUnofficialPFP} alt="Golden TBA logo" className="pfp circle" />
+                    <div className="icons">
+                        <FontAwesomeIcon className="icon" icon={faDiscord} />
+                        <Image src={TBAUnofficialPFP} alt="Golden TBA logo" className="pfp circle" />
+                    </div>
                     <div className="desc">
                         <h2>TBAUnofficial</h2>
                         Developed by FRC member Brandon [@BrandgrandReal] for other FRC members to fetch information from TBA and my own DB.
